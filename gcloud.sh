124d8c63496f8e9c873aa31cbfe19146121e4c99
Kiril Zhelyazkov <kiril.zhelyazkov9@gmail.com>
2025-05-18 09:06:04 +0300

    removed gcloud dotfile as it makes managing gcloud more difficult than it should

--- gcloud.sh
+++
@@ -1,8 +0,0 @@
-gcloud() {
-  docker run --rm -it \
-    -v "$HOME/.config/gcloud":/root/.config/gcloud \
-    -v "$PWD":/workspace \
-    -w /workspace \
-    google/cloud-sdk:latest \
-    gcloud "$@"
-}
